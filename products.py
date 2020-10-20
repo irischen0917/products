3848150fcf8499f623e882d64fd17e359f4f3121
Iris Chen <jin2006613@gmail.com>
2020-10-20 15:15:54 +0800

    two dimensional list

---
+++ products.py
@@ -0,0 +1,10 @@
+#商品二維清單
+products = []
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格：')
+	products.append([name, price])
+print(products)
+print(products[0][0], '的價格為', products[0][1])
